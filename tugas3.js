febf66017de059ad5cf94053ca0233b763b24f7a
wiladhianty <wiladhianty@gmail.com>
2021-06-30 10:56:34 +0700

    tambah tugas

---
+++ tugas3.js
@@ -0,0 +1,10 @@
+function vol_kubus(sisi){
+    let vol = sisi*sisi*sisi
+    return vol
+}
+
+var s = 2
+var volume = vol_kubus(s)
+
+console.log("Nilai sisi kubus : ", s)
+console.log("Hasil kalkulator volume kubus : ", volume)
